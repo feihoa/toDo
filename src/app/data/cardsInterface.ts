02d089767311d3d21eb28322b2db4384674712d3
Maria Terenteva <rtillkriyt@gmail.com>
2021-11-07 12:49:32 +0300

    fixes

--- src/app/data/cardsInterface.ts
+++ src/app/data/cardsInterface.ts
@@ -3,5 +3,12 @@ import { TasksInterface } from './tasksInterface';
 export interface CardsInterface {
   id:number;
   title: string;
-  todos:TasksInterface;
+  todos:TasksInterface[];
 }
+
+export interface CardsConstructor {
+  new ( id:number,  title: string, todos:TasksInterface): CardsInterface;
+  clone(): CardsInterface;
+}
+
+export let CardsInterface: CardsConstructor;
--- src/app/data/tasksInterface.ts
+++ src/app/data/tasksInterface.ts
@@ -3,3 +3,10 @@ export interface TasksInterface {
   text: string;
   isCompleted: boolean;
 }
+
+export interface TasksConstructor {
+  new (  id:number, text: string, isCompleted: boolean): TasksInterface;
+  clone(): TasksInterface;
+}
+
+export let TasksInterface: TasksConstructor;
